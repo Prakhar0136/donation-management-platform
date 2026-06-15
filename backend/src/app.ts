757b785e9f1645a919045837909d1b107c153461
Prakhar0136 <prakharps010306@gmail.com>
2026-06-15 12:15:37 +0530

    feat: add global error handling architecture

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import healthRoutes from "./routes/health.routes";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/api/v1/health", healthRoutes);
+
+app.use(errorMiddleware);
 
 export default app
